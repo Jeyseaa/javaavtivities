8c5a7d05f39111aa66393a03af3e379bea8a6cc8
John Carl Balatan <145192890+Jeyseaa@users.noreply.github.com>
2023-10-29 16:41:22 +0800

    Update MyDate.java

--- Task121/MyDate.java
+++ Task121/MyDate.java
@@ -2,10 +2,13 @@ package Task121;
 import Task121.DateTask;
 public class MyDate {
     public static void main(String[] args) {
-        DateTask date1 = new DateTask();
-        DateTask date2 = new DateTask();
-        System.out.println(date1.toString());
-        System.out.println(date2.toString());
-       // display leapyears
+        DateTask date1 = new DateTask(1,1,1978);
+        DateTask date2 = new DateTask(9,21,1984);
+
+        System.out.println("Date1: " + date1.toString());
+        System.out.println("Date2: " + date2.toString());
+
+        System.out.println("Leap years between 1980 and 2023:");
+        DateTask.leapYears();
     }
 }
