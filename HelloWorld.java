4b2fc017bae7548e6ffa0fffd37d28e98bf284a6
John Carl Balatan <145192890+Jeyseaa@users.noreply.github.com>
2023-10-29 15:23:05 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,9 @@
  * simply prints "Hello World!" to standard output.
  */
 class HelloWorldApp {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!); // Display the string.
+        System.out.println("Kamusta Mundo!"); // Display the string.
+
     }
 }
