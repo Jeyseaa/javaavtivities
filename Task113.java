e700e2b292c5f996e2c8d3dbee0d4a213e3df71a
John Carl Balatan <145192890+Jeyseaa@users.noreply.github.com>
2023-11-04 13:06:37 +0800

    Update Task113.java

--- Task113.java
+++ Task113.java
@@ -1,9 +1,9 @@
 class Task113 {
     public static void main (String[] args){
-        int Check_Number=10;
+      int check_number = 10;
         String message;
-        for (int i=1; i<= Check_Number; i++){
-            message = (i%2==0) ? i+ " is even number" : i+ " is odd number";
+        for (int i = 1; i <= check_number; i++) {
+            message = (i % 2 == 0) ? i + " is even number" : i + " is odd number";
             System.out.println(message);
         }
     }
