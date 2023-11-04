1dd5d6c92e2636cf1af00365c425114b713151c0
John Carl Balatan <145192890+Jeyseaa@users.noreply.github.com>
2023-11-04 13:16:45 +0800

    Update Grade_Score.java

--- ProgrammingChallenge/Grade_Score.java
+++ ProgrammingChallenge/Grade_Score.java
@@ -11,11 +11,11 @@ public class Grade_Score {
 
         if (Score >= 90) {
             System.out.println("Grade: A");
-        } else if (Score >= 80 && Score < 90) {
+        } else if (Score >= 80 && Score <= 89) {
             System.out.println("Grade: B");
-        } else if (Score >= 70 && Score < 80) {
+        } else if (Score >= 70 && Score <= 79) {
             System.out.println("Grade: C");
-        } else if (Score >= 69 && Score < 70) {
+        } else if (Score >= 60 && Score <= 69) {
             System.out.println("Grade: D");
         } else {
             System.out.println("Grade F");
