659fe4b84a3181a2689fd502fb3ca1e16bf0a805
John Carl Balatan <145192890+Jeyseaa@users.noreply.github.com>
2023-11-05 11:47:05 +0800

    Update Multiplication_Table.java

--- ProgrammingChallenge/Multiplication_Table.java
+++ ProgrammingChallenge/Multiplication_Table.java
@@ -1,15 +1,19 @@
 package ProgrammingChallenge;
-import java.util.Scanner;
+
 public class Multiplication_Table {
     public static void main(String[] args) {
-        ROW:        for(int m = 1;m<=10;m++) {
-            COL:            for (int n = 1; n <= 10; n++) {
-                System.out.print(n*m+" ");
+        for (int m = 1; m <= 10; m++) {
+            // Print leading spaces to create a pyramid shape
+            for (int space = 1; space <= 10 - m; space++) {
+                System.out.print("   ");
+            }
+
+            for (int n = 1; n <= m; n++) {
+                int product = n * m;
+                // Print the product with leading spaces to align them
+                System.out.printf("%3d    ", product);
             }
             System.out.println();
         }
     }
-
-
 }
-
