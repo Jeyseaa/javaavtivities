8522268cb2eaee2efd8eac370feadd4645044316
John Carl Balatan <145192890+Jeyseaa@users.noreply.github.com>
2023-11-05 11:43:40 +0800

    Update Integer_ForLoop.java

--- ProgrammingChallenge/Integer_ForLoop.java
+++ ProgrammingChallenge/Integer_ForLoop.java
@@ -7,14 +7,20 @@ public class Integer_ForLoop {
         System.out.println("Enter an integer: ");
         int number = input.nextInt();
 
-        ROW:        for(int row = 1; row <=number; row++) {
-            COL:            for (int col = 1; col <= number; col++) {
-                System.out.print(" * ");
+        for (int row = 1; row <= number; row++) {
+            // Print spaces before the asterisks
+            for (int space = 1; space <= number - row; space++) {
+                System.out.print(" ");
+            }
+
+            // Print asterisks
+            for (int col = 1; col <= 2 * row - 1; col++) {
+                System.out.print("*");
             }
             System.out.println();
         }
-
     }
-
-
 }
+
+
+
