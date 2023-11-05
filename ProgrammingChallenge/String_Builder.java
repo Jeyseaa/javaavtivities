599ed8fbd2c55be3de37107a1a47485815143676
John Carl Balatan <145192890+Jeyseaa@users.noreply.github.com>
2023-11-05 14:30:13 +0800

    Update String_Builder.java

--- ProgrammingChallenge/String_Builder.java
+++ ProgrammingChallenge/String_Builder.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 
 public class String_Builder {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter a letter or string: ");
+        String inputString = scanner.nextLine();
 
-        StringBuilder strBuild = new StringBuilder(10);
+        StringBuilder strBuild = new StringBuilder(inputString);
 
-        System.out.println("The String characters are "  + strBuild.capacity());
-
+        System.out.println("The capacity of the StringBuilder for your input is " + strBuild.capacity());
+        System.out.println("The number of characters in the input is: " + strBuild.length());
     }
 }
