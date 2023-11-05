70adfc4f04ff6be50e5951128ed2f1983ec5912c
John Carl Balatan <145192890+Jeyseaa@users.noreply.github.com>
2023-11-05 13:50:18 +0800

    Update String_Methods.java

--- ProgrammingChallenge/String_Methods.java
+++ ProgrammingChallenge/String_Methods.java
@@ -1,11 +1,23 @@
 package ProgrammingChallenge;
 
+import java.util.Scanner;
+
 public class String_Methods {
     public static void main(String[] args) {
-          String FirstName = "John Carl ";
-        String MiddleName = "Miguel";
-        String LastName = "Balatan";
-        char result = MiddleName.charAt(0);
-        System.out.println("Fullname: " + FirstName + result + " " + LastName);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your First Name: ");
+        String FirstName = scanner.nextLine();
+
+        System.out.print("Enter your Middle Name: ");
+        char MiddleName = scanner.nextLine().charAt(0);
+
+        System.out.print("Enter your Last Name: ");
+        String LastName = scanner.nextLine();
+
+
+
+
+        System.out.println("Fullname: " + FirstName +" " + MiddleName + " " + LastName);
     }
 }
