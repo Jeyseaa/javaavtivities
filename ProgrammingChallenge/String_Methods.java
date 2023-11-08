d0bd5a3b633466812e7df5100942b72d98597197
John Carl Balatan <145192890+Jeyseaa@users.noreply.github.com>
2023-11-07 21:21:12 -0800

    Update String_Methods.java

--- ProgrammingChallenge/String_Methods.java
+++ ProgrammingChallenge/String_Methods.java
@@ -6,37 +6,31 @@ public class String_Methods {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter your First Name: ");
+        System.out.print("Please Enter a String or Letters: ");
         String FirstName = scanner.nextLine();
-char Firstname = FirstName.charAt(0);
-int firstname = FirstName.length() - 1;
-int first= 3;
-int last= 6;
-        String substringOfFirstName = FirstName.substring(first, last);
+         if (FirstName.isEmpty() || FirstName.length() < 6){
+            System.out.println("The Given input is empty or too short " + "(Please Try Again)");
+
+        } else {
+            char Firstname = FirstName.charAt(0);
+            int firstname = FirstName.length() - 1;
+            int first = 1;
+            int last = 6;
+            String substringOfFirstName = FirstName.substring(first, last);
+
+            System.out.println("The String of your Given Input: " + FirstName);
+
+            int fullNameLength = FirstName.replaceAll("[^a-zA-Z]", "").length();
 
 
-
-        System.out.print("Enter your Middle Name: ");
-        String MiddleName = scanner.nextLine();
-
-        char MiddleIni = MiddleName.charAt(0);
-MiddleIni= Character.toUpperCase(MiddleIni);
-
-        System.out.print("Enter your Last Name: ");
-        String LastName = scanner.nextLine();
-
-        System.out.println("The String of your Full Name is: " + FirstName +" " + MiddleIni + " " + LastName);
-
-        int fullNameLength = FirstName.replaceAll("[^a-zA-Z]", "").length() +
-                LastName.replaceAll("[^a-zA-Z]", "").length() + 1;
+            System.out.println("This is String length of your Input: " + fullNameLength);
+            System.out.println("This is String UpperCase of your Input: " + FirstName.toUpperCase());
+            System.out.println("This is String UpperCase of your Input: " + FirstName.toLowerCase());
+            System.out.println("This is First Character of your Input: " + Firstname);
+            System.out.println("This is Last Character of your Input: " + FirstName.charAt(firstname));
+            System.out.println("This is Sub String of your Input: " + substringOfFirstName);
 
 
-
-        System.out.println("This is String length of your Fullname: " + fullNameLength);
-        System.out.println("This is String UpperCase of your Fullname: " +  FirstName.toUpperCase() +" " + MiddleIni + " " + LastName.toUpperCase());
-        System.out.println("This is First Character of your First Name: "  + Firstname);
-        System.out.println("This is Last Character of your First Name: "  + FirstName.charAt(firstname));
-        System.out.println("This is Sub String of your First Name: " + substringOfFirstName);
-
+        }
     }
 }
