02e769c2276140aeebfa5558cb9cd8431db3d4c4
John Carl Balatan <145192890+Jeyseaa@users.noreply.github.com>
2023-11-08 01:48:50 -0800

    Update String_Methods.java

--- ProgrammingChallenge/String_Methods.java
+++ ProgrammingChallenge/String_Methods.java
@@ -7,30 +7,29 @@ public class String_Methods {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Please Enter a String or Letters: ");
-        String FirstName = scanner.nextLine();
-         if (FirstName.isEmpty() || FirstName.length() < 6){
-            System.out.println("The Given input is empty or too short " + "(Please Try Again)");
+        String input = scanner.nextLine();
 
+        if (input.isEmpty()) {
+            System.out.println("The Given input is empty. (Please Try Again.) ");
+        } else if (input.length() < 5) {
+            System.out.println("The Given input is too short. (Please Input atleast 5 characters again.) ");
+        } else if (!input.matches("^[a-zA-Z]*$")) {
+            System.out.println("The Given input contains special characters. (Please Input Letters or String only.");
         } else {
-            char Firstname = FirstName.charAt(0);
-            int firstname = FirstName.length() - 1;
-            int first = 1;
-            int last = 6;
-            String substringOfFirstName = FirstName.substring(first, last);
+            char firstCharacter = input.charAt(0);
+            int inputLength = input.length();
+            int lastIndex = inputLength - 1;
+            int start = 1;
+            int end = 5;
+            String substringOfInput = input.substring(start, Math.min(end, inputLength));
 
-            System.out.println("The String of your Given Input: " + FirstName);
-
-            int fullNameLength = FirstName.replaceAll("[^a-zA-Z]", "").length();
-
-
-            System.out.println("This is String length of your Input: " + fullNameLength);
-            System.out.println("This is String UpperCase of your Input: " + FirstName.toUpperCase());
-            System.out.println("This is String UpperCase of your Input: " + FirstName.toLowerCase());
-            System.out.println("This is First Character of your Input: " + Firstname);
-            System.out.println("This is Last Character of your Input: " + FirstName.charAt(firstname));
-            System.out.println("This is Sub String of your Input: " + substringOfFirstName);
-
-
+           
+            System.out.println("This is String length of your Given Input: " + inputLength);
+            System.out.println("This is String UpperCase of your Given Input: " + input.toUpperCase());
+            System.out.println("This is String LowerCase of your Given Input: " + input.toLowerCase());
+            System.out.println("This is First Character of your Given Input: " + firstCharacter);
+            System.out.println("This is Last Character of your Given Input: " + input.charAt(lastIndex));
+            System.out.println("This is Sub String of your Given Input: " + substringOfInput);
         }
     }
 }
