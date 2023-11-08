9e4a0b62cbf817f607a703ab240dd158b691ca03
John Carl Balatan <145192890+Jeyseaa@users.noreply.github.com>
2023-11-08 01:43:31 -0800

    Update String_Builder.java

--- ProgrammingChallenge/String_Builder.java
+++ ProgrammingChallenge/String_Builder.java
@@ -5,39 +5,38 @@ public class String_Builder {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a letter or string: ");
+        System.out.print("Please Enter a Letter or String: ");
         String inputString = scanner.nextLine();
 
-        if (inputString.isEmpty() || inputString.length() < 10) {
-            System.out.println("The Given input is empty or too short " + "(Please Try Again)");
-
+        if (inputString.isEmpty()) {
+            System.out.println("The Given input is empty. (Please Try Again.) ");
+        } else if (inputString.length() < 10) {
+            System.out.println("The Given input is too short. (Please Input atleast 10 characters again.) ");
+        } else if (!inputString.matches("^[a-zA-Z]*$")) {
+            System.out.println("The Given input contains special characters. (Please Input Letters or String only.)");
         } else {
-            char inputstr = inputString.charAt(0);
-            int InputString = inputString.length() - 1;
-            int first = 2;
-            int last = 6;
-            String substringOfinputString = inputString.substring(first, last);
+            char firstCharacter = inputString.charAt(0);
+            int inputLength = inputString.length();
+            int lastIndex = inputLength - 1;
+            int start = 2;
+            int end = 6;
+            String substringOfInput = inputString.substring(start, Math.min(end, inputLength));
 
             StringBuilder strBuild = new StringBuilder(inputString);
 
-            System.out.println("The Number of Characters in the input is: " + strBuild.length());
-            System.out.println("This is First Character of your Input: " + inputstr);
-            System.out.println("This is Last Character of your Input: " + inputString.charAt(InputString));
-            System.out.println("This is Sub String of your Input: " + substringOfinputString);
-            System.out.println("The index of the first occurrence of 'a' in the input string is: " + strBuild.indexOf("a"));
+            System.out.println("This is Number of Characters in the Given input: " + strBuild.length());
+            System.out.println("This is First Character of your Given Input: " + inputString.charAt(0));
+            System.out.println("This is Last Character of your Given Input: " + inputString.charAt(lastIndex));
+            System.out.println("This is Index of the first occurrence of 'a': " + strBuild.indexOf("a"));
+            System.out.println("This is Sub String of your Given Input: " + substringOfInput);
+
             strBuild.append("123");
             strBuild.insert(4, "xyz");
             strBuild.delete(2, 5);
             strBuild.deleteCharAt(8);
             strBuild.reverse();
 
-            System.out.println("The String after appending '123': " + strBuild.toString());
-            System.out.println("The String After inserting 'xyz' at index 4: " + strBuild.toString());
-            System.out.println("The String After deleting the substring from index 2 to 4: " + strBuild.toString());
-            System.out.println("The String After deleting the character at index 8: " + strBuild.toString());
-            System.out.println("After reversing the string: " + strBuild.toString());
-
-
+            System.out.println("This is after reversing of Given Input: " + strBuild.toString());
         }
     }
 }
